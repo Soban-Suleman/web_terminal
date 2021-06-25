2f06927380cbc66f26060e4095c9981f098d3acf
Muhammad Soban <fa18-bcs-058@cuilahore.edu.pk>
2021-06-25 16:54:59 +0500

    fix

--- app/http/shopController.js
+++ app/http/shopController.js
@@ -1,11 +1,11 @@
-const Product = require("../../models/product");
-function shopController() {
-  return {
-    async shop(req, res) {
-      const products = await Product.find();
-      return res.render("shop", { products: products });
-    },
-  };
-}
+// const Product = require("../../models/product");
+// function shopController() {
+//   return {
+//     async shop(req, res) {
+//       const products = await Product.find();
+//       return res.render("shop", { products: products });
+//     },
+//   };
+// }
 
-module.exports = shopController;
+// module.exports = shopController;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-//const authController = require("../app/http/authController");
-// const cartController = require("../app/http/customers/cartController");
-const shopController = require("../app/http/shopController");
 const authentication = require("../app/middlewares/userAuthentication");
-// const orderController = require("../app/http/customers/orderController");
-
-//console.log(auth.userAuth);
 router.get("/", (req, res) => {
   res.render("index");
 });
-router.get("/shop", shopController().shop);
 
 router.get("/admin", authentication.checkAuthenticated, (req, res) => {
   res.render("admin");
@@ -27,15 +20,5 @@ router.post("/logout", (req, res) => {
 router.get("/register", authentication.checkNotAuthenticated, (req, res) => {
   res.render("register");
 });
-
-//Customer Routes
-// router.post("/orders", orderController().order);
-// router.get("/order", orderController().index);
-
-// // router.get("*", (req, res) => {
-// //   res.render("404", {
-// //     errorcomment: "Opps, Page Not Found",
-// //   });
-// // });
 
 module.exports = router;
